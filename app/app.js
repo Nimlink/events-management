a0441a39512074c78455aef623d575feb82719e2
Julien <Julien.sites@gmail.com>
2016-03-06 14:09:31 +0100

    angular-google-analytics

--- app/app.js
+++ app/app.js
@@ -1,11 +1,17 @@
-(function () {
-    angular.module('fup', [
-        'ui.router',
-        'ui.bootstrap',
-        'pascalprecht.translate',
-        'angular-input-stars',
-        'ui.knob',
-        'ngSanitize',
-        'ui.select'
-    ])
-})();
+var app = angular.module('fup', [
+    'ui.router',
+    'ui.bootstrap',
+    'pascalprecht.translate',
+    'angular-input-stars',
+    'ui.knob',
+    'ngSanitize',
+    'ui.select',
+    'angular-google-analytics'
+]);
+
+app.config(function (AnalyticsProvider) {
+    // Set a single account
+    AnalyticsProvider.setAccount('UA-74203547-1');
+});
+
+app.run(function(Analytics) {});
